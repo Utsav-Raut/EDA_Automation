1b44b33fd9b491ef38e1f5a7a5cbef4052d43e37
Utsav <utsav.raut@yahoo.com>
2019-03-12 18:01:03 +0530

    Update and rename file_ops.py to file_operations.py

--- nike/utils/file_operations.py
+++ nike/utils/file_operations.py
@@ -2,7 +2,7 @@ import findspark
 findspark.init()
 from pyspark.sql import SparkSession
 
-def get_files_details():
+def get_file_row_count():
     spark = SparkSession.builder.appName("Anything").getOrCreate()
     # df = spark.read.json('D:/Users/URaut/Pictures/Files_for_Infosys/')
     # tra_list= df.select('notifications.POSLog.Transaction.TransactionID').collect()
@@ -10,6 +10,4 @@ def get_files_details():
     tra_list= df.select('salary').collect()
     tra_list
     # tra_list[0].TransactionID
-    print(len(tra_list))
     return len(tra_list)
-# get_files_details()
