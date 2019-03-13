c257f0f2ed2e3e6b923d92f4d4c62b2c8fe8e23c
Utsav <utsav.raut@yahoo.com>
2019-03-13 16:45:37 +0530

    Update etl_job.py

--- nike/etl_job.py
+++ nike/etl_job.py
@@ -44,20 +44,15 @@ def transformed_data(df, spark, column_with_agg_func, table_name, field_name,
     row_count_diff, result, hive_tbl_row_count, input_file_row_count = get_row_count_diff(spark, df, field_name, table_name)
     agg_output = get_agg_result(df, column_with_agg_func)
     list_of_col_values_after_null_check = check_data_quality(spark, df, table_name, column_to_retrieve_after_null_check, columns_to_check_for_null, check_type)
-    # length_of_null_values = len(list_of_col_values_after_null_check)
-    # # print(list_of_col_values_after_null_check.head()[length_of_null_values])
-    # print(length_of_null_values)
     for x in list_of_col_values_after_null_check:
-        x.show()
-    #     print(x.count())
-    #     # print(x.get[0])
-    #     print(x.head())
-    df_transformed = [row_count_diff, result, hive_tbl_row_count, input_file_row_count, agg_output]
+        print(x)
+    df_transformed = [row_count_diff, result, hive_tbl_row_count, input_file_row_count, agg_output, 
+                        list_of_col_values_after_null_check]
     return df_transformed
 
 def load_data(df_transformed, table_name):
     print('Hello')
-    # generate_report(df_transformed, table_name)
+    generate_report(df_transformed, table_name)
 
 # Entry point for PySpark ETL application
 if __name__ == '__main__':
