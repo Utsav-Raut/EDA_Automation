63bd9471635a287b3573fe0fd5914e608c46ce69
Utsav <utsav.raut@yahoo.com>
2019-03-20 14:47:31 +0530

    Update etl_job.py

--- nike/etl_job.py
+++ nike/etl_job.py
@@ -4,6 +4,7 @@ from pyspark.sql.types import DoubleType, IntegerType
 from utils.row_count_validation import get_row_count_diff
 from utils.data_quality import check_data_quality
 from utils.get_report import generate_report
+from utils.data_comparison import compare_data
 def main():
     """Main ETL script definition.
     :return: None
@@ -18,10 +19,10 @@ def main():
 
     # Execute ETL pipeline
     data = extract_data(spark, config['db_name'], config['table_name'])
-    data_transformed = transformed_data(data, spark, config['column_with_agg_func'], 
-                        config['table_name'], config['field_name'],
-                        config['column_to_retrieve_after_null_check'],
-                        config['columns_to_check_for_null'],
+    data_transformed = transformed_data(data, spark, config['table_name'], config['file_path'], 
+                        config['field_name'], config['column_with_agg_func'],
+                        config['fields_to_compare'], config['path_to_report_directory'], 
+                        config['column_to_retrieve_after_null_check'], config['columns_to_check_for_null'], 
                         config['check_type'])
     load_data(data_transformed, config['table_name'])
 
@@ -38,16 +39,21 @@ def extract_data(spark, db_name, table_name):
     )
     return df
 
-def transformed_data(df, spark, column_with_agg_func, table_name, field_name, 
-                    column_to_retrieve_after_null_check, columns_to_check_for_null, check_type):
+def transformed_data(df, spark, table_name, file_path, field_name, 
+                    column_with_agg_func, fields_to_compare, path_to_report_directory, column_to_retrieve_after_null_check,
+                    columns_to_check_for_null, check_type):
     "Transform the original data set"
-    row_count_diff, result, hive_tbl_row_count, input_file_row_count = get_row_count_diff(spark, df, field_name, table_name)
-    agg_output = get_agg_result(df, column_with_agg_func)
-    list_of_col_values_after_null_check = check_data_quality(spark, df, table_name, column_to_retrieve_after_null_check, columns_to_check_for_null, check_type)
-    for x in list_of_col_values_after_null_check:
+    row_count_diff, result, hive_tbl_row_count, input_file_row_count = get_row_count_diff(spark, df, field_name, table_name, file_path)
+    agg_output = get_agg_result(df, spark, table_name, column_with_agg_func)
+    comp_data = compare_data(spark, df, fields_to_compare)
+    list_of_rows_with_null_data, comments = check_data_quality(spark, df, table_name, column_to_retrieve_after_null_check, columns_to_check_for_null, check_type)
+    for x in list_of_rows_with_null_data:
         print(x)
+    print(comments)
     df_transformed = [row_count_diff, result, hive_tbl_row_count, input_file_row_count, agg_output, 
-                        list_of_col_values_after_null_check]
+                        list_of_rows_with_null_data, comments, comp_data]
+    # df_transformed = [row_count_diff, result, hive_tbl_row_count, input_file_row_count, agg_output, comp_data]
+    # df_transformed = df
     return df_transformed
 
 def load_data(df_transformed, table_name):
