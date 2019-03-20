2954fdf51c404403abe2ea416d9d174f81704b50
Utsav <utsav.raut@yahoo.com>
2019-03-20 14:53:42 +0530

    Update get_report.py

--- nike/utils/get_report.py
+++ nike/utils/get_report.py
@@ -10,8 +10,10 @@ def generate_report(data, table_name):
     input_file_row_count = data[3]
     aggregate_result = data[4] 
     null_check_data = data[5]
+    comments_after_null_chk = data[6]
+    comp_result_list = data[7]
 
-    with open("D:/Users/URaut/Desktop/Agg_Results.csv", "w") as csvfile:
+    with open("D:/Users/URaut/Desktop/utilities/reports/Agg_Results.csv", "w") as csvfile:
         fieldnames = ['Table', 'Column', 'Agg_Func', 'Agg_Val']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
         writer.writeheader()   
@@ -21,7 +23,7 @@ def generate_report(data, table_name):
                     print("{0} columns's {1} value is {2}".format(col_name, agg_opn_name, agg_val))
                     writer.writerow({'Table': table_name, 'Column': col_name, 'Agg_Func': agg_opn_name, 'Agg_Val': agg_val})
 
-    with open("D:/Users/URaut/Desktop/Row_count_val_Results.csv", "w") as csvfile:
+    with open("D:/Users/URaut/Desktop/utilities/reports/Row_count_val_Results.csv", "w") as csvfile:
         fieldnames = ['Number of records from POS', 'Number of transactions loaded into stage', 'Difference', 'Comments']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
         writer.writeheader()   
@@ -29,17 +31,23 @@ def generate_report(data, table_name):
                         'Number of transactions loaded into stage': hive_tbl_row_count,
                         'Difference': difference, 'Comments': comments})
 
-    with open("D:/Users/URaut/Desktop/Null_check_Results.csv", "w") as csvfile:
-        fieldnames = ['Table', 'Col Checked For Null', 'Column', 'Column_Values']
+    with open("D:/Users/URaut/Desktop/utilities/reports/Null_check_Results.csv", "w") as csvfile:
+        fieldnames = ['Table', 'Comments', 'TransactionIDs']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
-        writer.writeheader()   
-        for result in null_check_data:
-            for col_name, col_values in result.items():
-                writer.writerow({'Table': table_name,  'Col Checked For Null': col_values[0], 'Column': col_name})
-                for val in range(1, len(col_values)):
-                    # print("{0} columns's value is {1}".format(col_name, val))
-                    writer.writerow({'Column_Values': col_values[val]})
+        writer.writeheader()
+        writer.writerow({'Table': table_name,  'Comments': comments_after_null_chk})   
+        for data in null_check_data:
+            writer.writerow({'TransactionIDs': data})
 
+    with open("D:/Users/URaut/Desktop/utilities/reports/Compare_Results.csv", "w") as csvfile:
+        fieldnames = ['TransactionID','Table_data_SequenceNumber', 'File_data_SequenceNumber']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',')
+        writer.writeheader()
+        for result in comp_result_list:
+            list_of_keys = []
+            for key, val in result.items():
+                list_of_keys.append(key)            
+            writer.writerow({'TransactionID': result.get(list_of_keys[0]) , 'Table_data_SequenceNumber': result.get(list_of_keys[1]), 'File_data_SequenceNumber': result.get(list_of_keys[2])})
 
     
     
