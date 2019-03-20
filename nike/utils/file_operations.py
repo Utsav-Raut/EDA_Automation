d3d23859f55e8060e18f67fcea8cf8eb5c90fe11
Utsav <utsav.raut@yahoo.com>
2019-03-20 14:52:58 +0530

    Update file_operations.py

--- nike/utils/file_operations.py
+++ nike/utils/file_operations.py
@@ -2,12 +2,8 @@ import findspark
 findspark.init()
 from pyspark.sql import SparkSession
 
-def get_file_row_count():
-    spark = SparkSession.builder.appName("Anything").getOrCreate()
-    # df = spark.read.json('D:/Users/URaut/Pictures/Files_for_Infosys/')
-    # tra_list= df.select('notifications.POSLog.Transaction.TransactionID').collect()
-    df = spark.read.json('D:/Users/URaut/Videos/ETL-master/people')
-    tra_list= df.select('salary').collect()
-    tra_list
-    # tra_list[0].TransactionID
+def get_file_row_count(file_path, field_name):
+    spark = SparkSession.builder.appName("Read File Data").getOrCreate()
+    read_file_df = spark.read.json(file_path)
+    tra_list= read_file_df.select(field_name).collect()
     return len(tra_list)
